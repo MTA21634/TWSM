30a8a94b08f9ad177bd5bd87f6dbd957cdb57342
arijus <arijusgrotuzas@gmail.com>
2021-04-27 10:47:04 +0200

    Added animations to more pages

--- js/Tab3.js
+++ js/Tab3.js
@@ -2,7 +2,6 @@ $(function() {
 	$('.tab_three').hide().fadeIn(700);
 	$('.create_chat').hide().delay(700).fadeIn(700);
 
-
 	$( document ).tooltip({
       	position: {
       		my: "center top",
@@ -10,11 +9,28 @@ $(function() {
       	}
     });
 
-	$('#join_now').click(function() {
-		document.location.href = 'tab4.php';
+    $('#small_button').click(function() {
+		var name = document.getElementById('name');
+		var number = document.getElementById('number');
+
+		if (name.value != "" && number.value != "")
+		{
+			document.location.href = 'tab5.html';
+		}
+		else
+		{
+			var options = {};
+			$('#warning').text("Please, enter a valid username and a password.").addClass('warning').effect("shake", options, 500, callback);
+		}
 	});
 
-	$('#create_now').click(function() {
+	$('#small_button_back').click(function() {
 		document.location.href = 'tab3.php';
 	});
+
+	function callback() {
+      setTimeout(function() {
+        $( "#effect" ).removeAttr( "style" ).hide().fadeIn(700);
+      }, 1000 );
+    };
 });
--- tab3.php
+++ tab3.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8" />
 	<title>iChat</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -22,13 +23,14 @@
 <div class="create_chat">
   <form action="create.php" method="post">
     <label for="chat_room_name"></label>
-    <input type="text" id="name" name="chat_room_name" required placeholder="Name/ topic...">
+    <input type="text" id="name" name="chat_room_name" required placeholder="Name/ topic..." title = "Enter a name for your chat.">
     <br>
-    <input type="submit" name="submit" id="small_button" value="Create">
+    <input type="submit" name="submit" id="submit_button" value="Create">
   </form>
 </div>
 <input type="submit" id="small_button_back" value="Back">
 <script src="js/jquery-1.11.0.js"></script>
-<script src="js/Tab1.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="js/Tab3.js"></script>
 </body>
 </html>
